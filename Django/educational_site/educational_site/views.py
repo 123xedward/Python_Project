530feb54f1ff0c05c2946ca742071e39110e9154
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-02-06 15:19:16 -0600

    Django II
    Mas opciones Django

--- Django/educational_site/educational_site/views.py
+++ Django/educational_site/educational_site/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse('Hola Django')
+    return render(request, 'home.html')
